ad194a7a0caec66c643aa3b049fcdc8d9974e9bb
Davtia <grdzelishvilidaviti@gmail.com>
2024-02-25 18:16:55 +0400

    correct jsn iles

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/modern-websiete-hero./',
+  base: '/react-app-1/',
   plugins: [react()],
 })
